beb43f9663e483aaf4e752a46ccecc309ba5d7c5
artem <neznajkinartem357@gmail.com>
2020-03-21 19:28:18 +0600

    Добавьте файлы проекта.

---
+++ MediaPlayer/AudioTime.cs
@@ -0,0 +1,137 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace MediaPlayer
+{
+    public delegate void StartTimer(AudioFileReader audioFile);
+
+    public class AudioTime : INotifyPropertyChanged
+    {
+        public StartTimer Start;
+        private DispatcherTimer timer;
+        private int totalMinutes;
+        private int totalSeconds;
+        private int currentMinutes;
+        private int currentSeconds;
+        private string timerText;
+
+        public AudioTime()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(1000);
+            timer.Tick += new EventHandler(Tick);
+            Start += StartTimerNew;
+        }
+
+        private void StartTimerNew(AudioFileReader audioFile)
+        {
+            TotalMinutes = audioFile.TotalTime.Minutes;
+            TotalSeconds = audioFile.TotalTime.Seconds;
+            CurrentMinutes = 0;
+            CurrentSeconds = 0;
+            TimerText = $"00:00/{TotalMinutes}:{TotalSeconds}";
+            timer.Start();
+            Start -= StartTimerNew;
+            Start += StartTimerPause;
+        }
+
+        private void StartTimerPause(AudioFileReader audioFile)
+        {
+            CurrentSeconds++;
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+            TotalMinutes = 0;
+            TotalSeconds = 0;
+            CurrentMinutes = 0;
+            CurrentSeconds = 0;
+            Start -= StartTimerPause;
+            Start += StartTimerNew;
+        }
+
+        public void Pause()
+        {
+            timer.Stop();
+        }
+
+        private void Tick(object sender, EventArgs e)
+        {
+            if (CurrentSeconds == 59) CurrentMinutes++;
+            CurrentSeconds = CurrentSeconds == 59 ? 0 : CurrentSeconds + 1;
+            TimerText = $"{CurrentMinutes}:{CurrentSeconds}/{TotalMinutes}:{TotalSeconds}";
+            if (TotalMinutes == CurrentMinutes && TotalSeconds == CurrentSeconds)
+            {
+                timer.Stop();
+                Start -= StartTimerPause;
+                Start += StartTimerNew;
+            }
+        }
+
+        public int TotalMinutes
+        {
+            get { return totalMinutes; }
+            set
+            {
+                totalMinutes = value;
+                OnPropertyChanged("TotalMinutes");
+            }
+        }
+
+        public int TotalSeconds
+        {
+            get { return totalSeconds; }
+            set
+            {
+                totalSeconds = value;
+                OnPropertyChanged("TotalSeconds");
+            }
+        }
+
+        public int CurrentMinutes
+        {
+            get { return currentMinutes; }
+            set
+            {
+                currentMinutes = value;
+                OnPropertyChanged("CurrentMinutes");
+            }
+        }
+
+        public int CurrentSeconds
+        {
+            get { return currentSeconds; }
+            set
+            {
+                currentSeconds = value;
+                OnPropertyChanged("CurrentSeconds");
+            }
+        }
+
+        public string TimerText
+        {
+            get { return timerText; }
+            set
+            {
+                timerText = value;
+                OnPropertyChanged("TimerText");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop="")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ MediaPlayer/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System.ComponentModel;
+
+namespace MediaPlayer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        WaveOutEvent outputDevice;
+        AudioFileReader audioFile;
+        AudioTime musicTime;
+        string playingMusicFileName;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            musicTime = (AudioTime)this.Resources["MusicTimer"];
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void HideWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void CloseWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void RestoreWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            if (WindowState == WindowState.Normal)
+            {
+                bitmapImage.UriSource = new Uri("pack://application:,,,/MediaPlayer;component/window_control_images/normal-window.png");
+                WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                bitmapImage.UriSource = new Uri("pack://application:,,,/MediaPlayer;component/window_control_images/maximize-window.png");
+                WindowState = WindowState.Normal;
+            }
+            bitmapImage.EndInit();
+            RestoreWindowImage.Source = bitmapImage;
+        }
+
+        private void ChooseMusicButton_Click(object sender, RoutedEventArgs e)
+        {
+            StopMusic();
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "MP3 files|*.mp3";
+            if(fileDialog.ShowDialog() == true) playingMusicFileName = fileDialog.FileName;
+            MusicNameTextBox.Text = GetMusicName(playingMusicFileName);
+        }
+
+        private string GetMusicName(string path)
+        {
+            int slashIndex = 0;
+            for (int i = path.Length - 1; i > 0; i--)
+            {
+                if (path[i] == '\\')
+                {
+                    slashIndex = path.Length - i;
+                    break;
+                }
+            }
+            return path.Substring(slashIndex, path.Length - slashIndex);
+        }
+
+        private void PlayPauseMusicButton_Click(object sender, RoutedEventArgs e)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            if(outputDevice == null || outputDevice.PlaybackState == PlaybackState.Paused)
+            {
+                PlayMusic();
+                bitmapImage.UriSource = new Uri("pack://application:,,,/MediaPlayer;component/music_control_images/stop-music.png");
+                musicTime.Start(audioFile);
+            }
+            else
+            {
+                PauseMusic();
+                bitmapImage.UriSource = new Uri("pack://application:,,,/MediaPlayer;component/music_control_images/play-music.png");
+                musicTime.Pause();
+            }
+            bitmapImage.EndInit();
+            PlayPauseMusicImage.Source = bitmapImage;
+        }
+
+        private void PlayMusic()
+        {
+            if(outputDevice == null)
+            {
+                outputDevice = new WaveOutEvent();
+                outputDevice.PlaybackStopped += PlaybackStoped;
+            }
+            if(audioFile == null)
+            {
+                audioFile = new AudioFileReader(playingMusicFileName);
+                outputDevice.Init(audioFile);
+            }
+            outputDevice.Play();
+        }
+
+        private void PauseMusic()
+        {
+            outputDevice?.Pause();
+        }
+
+        private void StopMusic()
+        {
+            outputDevice?.Stop();
+        }
+
+        private void PlaybackStoped(object sender, StoppedEventArgs e)
+        {
+            outputDevice?.Dispose();
+            outputDevice = null;
+            audioFile?.Dispose();
+            audioFile = null;
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri("pack://application:,,,/MediaPlayer;component/music_control_images/play-music.png");
+            image.EndInit();
+            PlayPauseMusicImage.Source = image;
+        }
+
+        private void PlaybackStoped(object sender, CancelEventArgs e)
+        {
+            outputDevice?.Dispose();
+            outputDevice = null;
+            audioFile?.Dispose();
+            audioFile = null;
+        }
+    }
+}
