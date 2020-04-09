3064beefc7921e1580bd3dd31a6d80aaf4f9b3d4
artem <neznajkinartem357@gmail.com>
2020-04-09 12:10:28 +0600

    Added Playlist ViewModel

---
+++ MediaPlayer/view_models/Playlist.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPlayer.view_models
+{
+    class Playlist : INotifyPropertyChanged
+    {
+        public List<AudioRecord> AudioRecords { get; set; }
+        public AudioRecord CurrentRecord
+        { 
+            get
+            {
+                return currentRecord;
+            }
+            set
+            {
+                currentRecord = value;
+                OnPropertyChanged("CurrentRecord");
+            }
+        }
+        public int CurrentRecordIndex { get; set; }
+        public AudioRecordCommand AddAudio
+        {
+            get
+            {
+                return addAudio ?? (addAudio = new AudioRecordCommand(obj =>
+                {
+                    AudioRecords.Add(obj as AudioRecord);
+                }));
+            }
+        }
+        public AudioRecordCommand RemoveAudio
+        {
+            get
+            {
+                return removeAudio ?? (removeAudio = new AudioRecordCommand(obj =>
+                {
+                    AudioRecords.Remove(AudioRecords.ElementAt((int)obj));
+                }));
+            }
+        }
+
+        AudioRecordCommand addAudio;
+        AudioRecordCommand removeAudio;
+        AudioRecord currentRecord;
+
+        public Playlist()
+        {
+            AudioRecords = new List<AudioRecord>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
